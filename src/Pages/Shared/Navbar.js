2ccef7f1d4ff7bcbb0c61e52741fa1b6d3cfb2fe
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-15 14:28:49 +0600

    Navication bar done

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
 import logo from '../../Images/logo2.png'
-import { MenuIcon, XIcon } from '@heroicons/react/solid';
+import { MenuIcon, ShoppingCartIcon, XIcon } from '@heroicons/react/solid';
 import CustomLink from './CustomLink';
 
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
     return (
-        <nav className='py-2 bg-white z-50 flex container mx-auto'>
+        <nav className='py-2 bg-white z-50 flex container items-center mx-auto'>
             <div>
-                <img src={logo} className='h-8 ml-4' alt="" />
+                <img src={logo} className='h-10' alt="" />
             </div>
             <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden ml-auto mr-4'>
-                {open ? <XIcon className='text-red-100'></XIcon> : <MenuIcon className='text-red-100'></MenuIcon>}
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
             </div>
-            <ul className={`bg-white md:flex justify-end absolute md:static w-full duration-300 ease-in ${open ? 'top-11' : 'top-[-220px]'}`}>
+            <ul className={`bg-white md:flex justify-end absolute md:static w-full duration-300 ease-in ${open ? 'top-14' : 'top-[-220px]'}`}>
                 <CustomLink to={'/'}>HOME</CustomLink>
-                <CustomLink to={'/reviews'}>REVIEWS</CustomLink>
-                <CustomLink to={'/dashboard'}>DASHBOARD</CustomLink>
-                <CustomLink to={'/blogs'}>BLOGS</CustomLink>
-                <CustomLink to={'/about'}>ABOUT</CustomLink>
+                <CustomLink to={'/dashboard'}>
+                    <div className='w-6 h-5 mt-1 mx-auto'>
+                        <ShoppingCartIcon></ShoppingCartIcon>
+                    </div>
+                </CustomLink>
+                <CustomLink to={'/blogs'}>Login</CustomLink>
+                <CustomLink to={'/about'}>Sign Up</CustomLink>
             </ul>
         </nav>
 
@@ -27,24 +30,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
-
-
-{/* <nav className='py-2 z-50 bg-white md:flex justify-between container mx-auto'>
-            <img src={logo} className='h-9 pl-4' alt="" />
-            <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden top-3 absolute right-4'>
-                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
-            </div>
-            <ul className={`md:flex bg-white justify-end absolute md:static w-full duration-300 ease-in ${open ? 'top-30' : 'top-[-220px]'}`}>
-                <CustomLink to={'/'}>Home</CustomLink>
-                <CustomLink to={'/reviews'}>
-                    <div className='w-6 h-6 mt-1 mx-auto'>
-                        <ShoppingCartIcon></ShoppingCartIcon>
-                    </div>
-                </CustomLink>
-                <CustomLink to={'/dashboard'}>Login</CustomLink>
-                <CustomLink to={'/blogs'}>Sign Up</CustomLink>
-            </ul>
-        </nav> */}
