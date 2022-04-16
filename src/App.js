9b62688d0a5686966dddfc34ac0db5c910e65585
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-16 19:35:45 +0600

    Login registration from designed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Cart from './Pages/Cart/Cart';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login';
 import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/cart' element={<Cart></Cart>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Images/logo2.png'
+
+const Login = () => {
+    return (
+        <div className='container mx-auto my-5 p-4'>
+            <img className='mx-auto h-20' src={logo} alt="" />
+            <div className='mx-auto mt-5 max-w-md text-center'>
+
+                <form>
+
+
+                    <input type="email" name="email" id="email" className='bg-gray-200 w-full p-3 focus:outline-red-300 text-gray-800 my-2 rounded-md' placeholder='Your Email' required />
+
+                    <input type="password" name="password" id="password" className='bg-gray-200 w-full p-3 focus:outline-red-300 text-gray-800 my-2 rounded-md' placeholder='Enter Password' required />
+
+
+
+                    <button type="submit" className='w-full p-3 rounded-md bg-red-500 hover:bg-red-600 text-yellow-100 my-3'>Login</button>
+
+                    <Link className='text-red-600' to={'/signup'}>Don't have an account.?</Link>
+                </form>
+
+            </div>
+        </div>
+    );
+};
+
+export default Login;
